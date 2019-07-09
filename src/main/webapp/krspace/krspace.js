47646b0f4fe4f5bd269893f1bbb6a568d88ab7de
LT <704885881@qq.com>
2019-07-09 20:33:14 +0800

    添加新页面

---
+++ src/main/webapp/krspace/krspace.js
@@ -0,0 +1,18 @@
+
+window.onload = function(){
+    openSwiper();
+};
+
+
+function openSwiper() {
+    new Swiper('#hit_swipe', {
+        loop: true,
+        autoplay: 7000,
+        pagination: '#hit_swipe_indicator',
+        autoplayDisableOnInteraction: false,
+        paginationClickable: true,
+        paginationBulletRender: function (swiper, index, className) {
+            return '<span class="' + className + ' hit_indicator_bullet">' + (index + 1) + '</span>';
+        }
+    });
+}
